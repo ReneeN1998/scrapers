7feb29e7c3649bcad7f69f69216937412c0934b6
Renee <79910937+ReneeN1998@users.noreply.github.com>
2021-11-06 20:43:05 +0100

    Update finalgamecrawler1.py

--- finalgamecrawler1.py
+++ finalgamecrawler1.py
@@ -6,7 +6,7 @@ from selenium.webdriver.chrome.options import Options
 chrome_options = Options()
 chrome_options.add_argument("--headless")
 chrome_options.add_argument("--no-sandbox")
-chrome_options.add_argument("window-size=1920,1080")
+chrome_options.add_argument("--start-fullscreen")
 
  
 url_list=[]
