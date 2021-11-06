d2c95a9ea712c07a4b74dc46d85f1c3e3b6c9935
Renee <79910937+ReneeN1998@users.noreply.github.com>
2021-11-06 21:20:27 +0100

    Update finalgamecrawler1.py

--- finalgamecrawler1.py
+++ finalgamecrawler1.py
@@ -10,6 +10,7 @@ chrome_options.add_argument("--no-sandbox")
 chrome_options.add_argument("--start-maximized")
 chrome_options.add_argument("--disable-dev-shm-usage")
 chrome_options.add_argument("--window-size=1920x1080")
+chrome_options.add_argument("--window-position=0,0")
 
  
 url_list=[]
