2268f51a89efc938c67affb306cac42a98a388f4
Renee <79910937+ReneeN1998@users.noreply.github.com>
2021-11-07 15:40:49 +0100

    Update finalgamecrawler1.py

--- finalgamecrawler1.py
+++ finalgamecrawler1.py
@@ -44,7 +44,7 @@ with open ('testlinks10.csv') as urls:
             print('Age check passed')
         except:
             print('No age restriction')
-        time.sleep(3)
+        time.sleep(5)
         try: 
             title = driver.find_element_by_xpath('.//*[@id="appHubAppName"]').text
         except:
@@ -104,15 +104,15 @@ with open ('testlinks10.csv') as urls:
         try:
             link2 = driver.find_element_by_css_selector('div.partnereventwebrowembed_LatestUpdateButton_1TRFt')
             link2.click()
-            time.sleep(1) 
+            time.sleep(3) 
             downbutton = driver.find_element_by_css_selector('div.apppartnereventspage_ScrollButton_1t_97.apppartnereventspage_Down_3VePR.apppartnereventspage_AnimIn_240i5')
             actions = ActionChains(driver)
             actions.click(downbutton)
-            time.sleep(1)
+            time.sleep(3)
             for i in range(1000):
                 actions.perform()
             downbutton = ''
-            time.sleep(1)
+            time.sleep(3)
             update_count = len(driver.find_elements_by_css_selector('div.apppartnereventspage_PartnerEvent_1KsYS.partnereventdisplay_InLibraryView_3_SEi'))
         except: 
             update_count = ''
