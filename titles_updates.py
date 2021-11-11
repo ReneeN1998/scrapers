0fcd93a37c8d0f1b185e9ce4a2aa7a0786925eb9
Renee <79910937+ReneeN1998@users.noreply.github.com>
2021-11-11 21:59:48 +0100

    Update titles_updates.py

--- titles_updates.py
+++ titles_updates.py
@@ -4,6 +4,13 @@ import time
 from selenium.webdriver.chrome.options import Options
 chrome_options = Options()
 chrome_options.add_argument("--headless")
+chrome_options.add_argument("--no-sandbox")
+chrome_options.add_argument("--start-maximized")
+chrome_options.add_argument("--disable-dev-shm-usage")
+chrome_options.add_argument("--window-size=1920x1080")
+chrome_options.add_argument("--window-position=0,0")
+chrome_options.add_argument("--incognito")
+chrome_options.add_argument("--disable-gpu")
 
 url_list=[]
 #getting all the urls
