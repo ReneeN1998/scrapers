5f1e4ff3fae110dacf329435100a0bc350665116
Renee <79910937+ReneeN1998@users.noreply.github.com>
2021-11-14 22:20:49 +0100

    Update finalgamecrawler4.py

--- finalgamecrawler4.py
+++ finalgamecrawler4.py
@@ -4,6 +4,7 @@ import time
 from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.chrome.options import Options
 chrome_options = Options()
+chrome_options.add_argument("--headless")
 chrome_options.add_argument("--no-sandbox")
 chrome_options.add_argument("--start-maximized")
 chrome_options.add_argument("--disable-dev-shm-usage")
