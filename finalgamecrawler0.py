852e9016c4887b1e4df26dd9ff5e14e1026878a2
Renee <79910937+ReneeN1998@users.noreply.github.com>
2021-11-14 19:26:12 +0100

    Update finalgamecrawler0.py

--- finalgamecrawler0.py
+++ finalgamecrawler0.py
@@ -16,7 +16,7 @@ chrome_options.add_argument("--disable-gpu")
  
 url_list=[]
 #getting all the urls
-with open ('0bcde_updates.csv') as urls:
+with open ('11ca.csv') as urls:
     for url in urls:
         # create driver object
         driver = webdriver.Chrome("/home/test/chromedriver",options=chrome_options)
@@ -134,7 +134,7 @@ with open ('0bcde_updates.csv') as urls:
 df = pd.DataFrame(url_list)
 
 #Save data
-df.to_csv('0bcde_udatesdata.csv', index = False, encoding='utf-8')
+df.to_csv('11ca_games.csv', index = False, encoding='utf-8')
 print('Finished!')
 
 
